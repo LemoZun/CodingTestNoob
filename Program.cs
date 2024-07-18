61dc53d6082cc10322c9e3b163e53d0d13005e61
LemoZun <marrzun@gmail.com>
2024-07-18 17:33:41 +0900

    feat : 두 수의 합
    문제1번

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CodingTestNoob
+{
+    internal class Program
+    {
+        public int solution1(int num1, int num2)
+        {
+            int answer = num1 + num2;
+            return answer;
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
