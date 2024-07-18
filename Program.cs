f965b25a05bfd25a19d899c058a2bd5296c1d08a
LemoZun <marrzun@gmail.com>
2024-07-18 17:45:53 +0900

    feat: 두 수의 곱

--- Program.cs
+++ Program.cs
@@ -12,6 +12,11 @@
             int answer = num1 - num2;
             return answer;
         }
+        public int solution3(int num1, int num2)
+        {
+            int answer = num1 * num2;
+            return answer;
+        }
 
         static void Main(string[] args)
         {
