9dc40d75d3b840bde2dd6e1e48f4f4c744bc4d41
LemoZun <marrzun@gmail.com>
2024-07-18 18:21:02 +0900

    feat : 두 수의 나눗셈

--- Program.cs
+++ Program.cs
@@ -2,30 +2,35 @@
 {
     internal class Program
     {
-        public int SumTwoNumbers(int num1, int num2)
+        static int Solution1(int num1, int num2)
         {
             int answer = num1 + num2;
             return answer;
         }
-        public int MinusTwoNumbers(int num1, int num2)
+        static int Solution2(int num1, int num2)
         {
             int answer = num1 - num2;
             return answer;
         }
-        public int MultiTwoNumbers(int num1, int num2)
+        static int Solution3(int num1, int num2)
         {
             int answer = num1 * num2;
             return answer;
         }
-        public int DivideTwoNumbers(int num1, int num2)
+        static int Solution4(int num1, int num2)
         {
             int answer = num1 / num2;
             return answer;
         }
+        static decimal Solution5(int num1, int num2)
+        {
+            return (int)((float)num1 / num2 * 1000);
+            //return 1000*num1/num2;
+        }
 
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
