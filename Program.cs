d2b4cd2424f47b2884341a4b7b23a03804a9af65
LemoZun <marrzun@gmail.com>
2024-07-18 17:44:46 +0900

    feat : 두 수의 차

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@
             int answer = num1 + num2;
             return answer;
         }
-
+        public int solution2(int num1, int num2)
+        {
+            int answer = num1 - num2;
+            return answer;
+        }
 
         static void Main(string[] args)
         {
